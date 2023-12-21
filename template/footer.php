f1e10a348b92f9f7b1354bd57aff23161b8d19fe
sadiqui <sadiquiabdelilah@gmail.com>
2023-12-21 14:29:34 +0100

    initial project version

---
+++ template/footer.php
@@ -0,0 +1,20 @@
+      	<hr>
+
+      	<footer>
+        	<div class="text-muted pull-left">
+            	<a style="color: skyblue" href="https://github.com/sadiqui"> SADIQUI </a>
+        	</div>
+        	<div class="text-muted pull-right" style="color: white">
+          		<a style="color: skyblue" href="admin.php">Admin Login</a>
+        	</div>
+      	</footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
